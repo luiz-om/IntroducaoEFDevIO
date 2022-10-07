a26bb6327764566729516a8ed9104243f704ea20
luiz-om <luizfernando_martins@hotmail.com>
2022-10-06 23:08:10 -0400

    Adicionar arquivos de projeto.

---
+++ Curso/Data/ApplicationContext.cs
@@ -0,0 +1,13 @@
+﻿using CursoEFCore.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace CursoEFCore.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data source=(localdb)\\MSSQLLocalDB; initial Catalog= CursoEFCore; Integrated Security=true");
+        }
+    }
+}
---
+++ Curso/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CursoEFCore.ValueObjects;
+
+Console.WriteLine("Hello, World!");
+
+var teste = StatusPedido.Analise;
+Console.WriteLine(teste);
