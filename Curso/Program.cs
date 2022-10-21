6f2ce956f0305d7787aaee04ac48e55fe76cd645
luiz-om <luizfernando_martins@hotmail.com>
2022-10-20 22:31:40 -0400

    Inserção em massa

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -15,17 +15,17 @@ static void InserirProduto()
         Valor = 10m,
         TipoProduto = TipoProduto.MercadoriaParaRevenda,
         Ativo = true
-    
+
     };
 
     using var db = new ApplicationContext();
     //db.Entry(produto).State = EntityState.Added;
     //db.Set<Produto>().Add(produto);
     //db.Add(produto);
-        db.Produtos.Add(produto);
+    db.Produtos.Add(produto);
 
-   var teste = db.SaveChanges();
-    if (teste ==1)
+    var teste = db.SaveChanges();
+    if (teste == 1)
     {
         Console.WriteLine($"If quantidade de registros inseridos{teste} ");
 
@@ -35,7 +35,57 @@ static void InserirProduto()
         Console.WriteLine($"Else quantidade de registros inseridos{teste} ");
     }
 
-   
-}
 
-InserirProduto();
+}
+static void InserirDadosEmMassa()
+{
+    //declaração para criar uma instância  produto
+    var produto = new Produto
+    {
+        Descricao = "Produto Teste",
+        CodigoBarras = "123455646411",
+        Valor = 10m,
+        TipoProduto = TipoProduto.MercadoriaParaRevenda,
+        Ativo = true
+
+    };
+
+    var cliente = new Cliente
+    {
+        Nome = "Luiz Fernando",
+        CEP = "79011210",
+        Cidade = "Campão",
+        Estado =  "MS",
+        Telefone = "67998153415"
+    };
+
+    var testeLista = new[]
+    {
+        new Cliente
+        {
+             Nome = "Luiz Fernando3",
+        CEP = "79011210",
+        Cidade = "Campão",
+        Estado =  "MS",
+        Telefone = "67998153415"
+        },new Cliente
+        {
+             Nome = "Luiz Fernando4",
+        CEP = "79011210",
+        Cidade = "Campão",
+        Estado =  "MS",
+        Telefone = "67998153415"
+        }
+    };
+
+    using var db = new ApplicationContext();
+    // Insere varios registros tanto separados ou uma lista.
+    db.AddRange(cliente, produto);
+
+    var registros = db.SaveChanges();
+    Console.WriteLine($"Total de Registro(s): {registros}");
+
+
+}
+//InserirProduto();
+InserirDadosEmMassa();
